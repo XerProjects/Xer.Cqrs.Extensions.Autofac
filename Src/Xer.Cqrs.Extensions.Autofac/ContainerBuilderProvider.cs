6cb161805adf4037737c8cfe6ee42563d60f8318
mvput <martijn@vanput.net>
2018-02-19 21:06:05 +0100

    Initial commit

---
+++ Src/Xer.Cqrs.Extensions.Autofac/ContainerBuilderProvider.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System.Collections.Generic;
+
+namespace Xer.Cqrs.Extensions.Autofac
+{
+    /// <summary>
+    /// Represents an adapter to <see cref="IComponentContext"/>.
+    /// </summary>
+    public class ContainerBuilderProvider : CommandStack.Resolvers.IContainerAdapter,
+                                            EventStack.Resolvers.IContainerAdapter
+    {
+        private readonly IComponentContext _context;
+
+        public ContainerBuilderProvider(IComponentContext context)
+        {
+            this._context = context;
+        }
+
+        public T Resolve<T>() where T : class => this._context.Resolve<T>();
+
+        public IEnumerable<T> ResolveMultiple<T>() where T : class => this._context.Resolve<IEnumerable<T>>();
+    }
+}
