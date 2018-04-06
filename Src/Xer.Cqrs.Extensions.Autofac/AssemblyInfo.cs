b22d0aa503b8e65af57b7f14e0b7b6fd647fe7d7
Martijn van Put <martijn@vanput.net>
2018-04-06 14:50:24 +0200

    Update AssemblyInfo.cs

--- Src/Xer.Cqrs.Extensions.Autofac/AssemblyInfo.cs
+++ Src/Xer.Cqrs.Extensions.Autofac/AssemblyInfo.cs
@@ -4,7 +4,3 @@ using System.Runtime.InteropServices;
 
 // Allow test project to access internal classes.
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Xer.Cqrs.Extensions.Autofac.Tests")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
