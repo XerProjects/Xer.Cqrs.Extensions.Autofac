1bef5b49e47816b745723e3c96fdddd0740560c8
Martijn van Put <martijn@vanput.net>
2018-04-06 14:48:28 +0200

    Create AssemblyInfo.cs

---
+++ Src/Xer.Cqrs.Extensions.Autofac/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allow test project to access internal classes.
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Xer.Cqrs.Extensions.Autofac.Tests")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
