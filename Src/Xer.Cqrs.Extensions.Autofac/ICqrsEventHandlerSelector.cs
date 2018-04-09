f868bf5fab6e0983415928f4b3ab8efc91d73411
Martijn van Put <martijn@vanput.net>
2018-04-09 10:17:43 +0200

    Fixes for release 1.0 (#5)
    * Fixes for release 1.0
    
    * Downgrade xer delegator
    
    * CqrsEventhandlerSelector API updates + csproj updates
    
    * Change ComponentContextAdapter to optionally resolve objects

--- Src/Xer.Cqrs.Extensions.Autofac/ICqrsEventHandlerSelector.cs
+++ Src/Xer.Cqrs.Extensions.Autofac/ICqrsEventHandlerSelector.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace Xer.Cqrs.Extensions.Autofac
 {
     public interface ICqrsEventHandlerSelector
     {
-        ICqrsEventHandlerSelector ByInterface(Assembly assembly);
-        ICqrsEventHandlerSelector ByInterface(Assembly assembly, Lifetime lifetime);
-        ICqrsEventHandlerSelector ByInterface(IEnumerable<Assembly> assemblies);
-        ICqrsEventHandlerSelector ByInterface(IEnumerable<Assembly> assemblies, Lifetime lifetime);
-        ICqrsEventHandlerSelector ByAttribute(Assembly assembly);
-        ICqrsEventHandlerSelector ByAttribute(Assembly assembly, Lifetime lifetime);
-        ICqrsEventHandlerSelector ByAttribute(IEnumerable<Assembly> assemblies);
-        ICqrsEventHandlerSelector ByAttribute(IEnumerable<Assembly> assemblies, Lifetime lifetime);
+        ICqrsEventHandlerSelector ByInterface(params Assembly[] assemblies);
+        ICqrsEventHandlerSelector ByInterface(Lifetime lifetime, params Assembly[] assemblies);
+        ICqrsEventHandlerSelector ByAttribute(params Assembly[] assemblies);
+        ICqrsEventHandlerSelector ByAttribute(Lifetime lifetime, params Assembly[] assemblies);
     }
 }
